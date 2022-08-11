819159f10e180eb31334b94dd3abdf5fd917ec35
shin <shin.shin.desktoppc@gmail.com>
2022-08-11 14:06:40 +0900

    [Add] create login functions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user_name = params[:name]
+    user = User.find_by(name: user_name)
+
+    if user && user.authenticate(params[:sessions][:password])
+      User.log_in(user)
+      redirect_to root_url, notice: "ログインしました"
+    else
+      redirect_to "login_url", alert: "ログイン名もしくはパスワードが間違っています"
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,6 +57,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
